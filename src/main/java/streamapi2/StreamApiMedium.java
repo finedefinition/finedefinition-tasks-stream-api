ae88988bb6afc77b5d4633ab1d6b2240060e658b
Sergii Bezrukov <73116558+finedefinition@users.noreply.github.com>
2022-12-09 13:18:39 +0000

    Task added with changes

--- src/main/java/streamapi2/StreamApiMedium.java
+++ src/main/java/streamapi2/StreamApiMedium.java
@@ -1,8 +1,9 @@
 package streamapi2;
 
-import java.util.Collections;
-import java.util.List;
-import java.util.Map;
+import java.util.*;
+import java.util.stream.Collectors;
+import java.util.stream.IntStream;
+import java.util.stream.Stream;
 
 public class StreamApiMedium {
     /**
@@ -13,8 +14,31 @@ public class StreamApiMedium {
      * will be generated. Result shouldn't contain duplicates.
      */
     public Long getVisitorsPerYear(List<String> records, int year) {
+        String yearToString = Integer.toString(year);
 
-        return 0L;
+        Map<String, String> stringStringMap = records.stream()
+                .map(str -> str.split(":"))
+                .filter(z -> z[1].equals(yearToString))
+                .collect(Collectors.toMap(a -> a[0], a -> a[1], (a1, a2) -> a1));
+
+        long result = stringStringMap.size();
+        return result;
+//        Map<String, Integer> map =  records.stream()
+//                .peek(System.out::println)
+//                .map(str -> str.split(":"))
+//                .peek(System.out::println)
+//                .collect(Collectors.toMap(str -> str[0], str -> Integer.getInteger(str[1])));
+//        System.out.println(map);
+//        long count = map.entrySet()
+//                .stream()
+//                .filter(e -> e.getValue().equals(year))
+//                .peek(System.out::println)
+//                .map(Map.Entry::getKey)
+//                .peek(System.out::println)
+//                .count();
+//
+//
+//        return count;
     }
 
     /**
@@ -24,7 +48,13 @@ public class StreamApiMedium {
      * Output : {"Odyssey", "Sun.ltd"}
      */
     public List<String> getCompanies(Map<String, Integer> input) {
-        return Collections.emptyList();
+        List<String> collect = input.entrySet()
+                .stream()
+                .filter(f -> f.getValue() > 0)
+                .map(b -> b.getKey())
+                .sorted()
+                .collect(Collectors.toList());
+        return collect;
     }
 
     /**
@@ -37,7 +67,11 @@ public class StreamApiMedium {
      * (100001)
      */
     public String convertAndModifyNumbers(List<Integer> numbers) {
-        return "";
+        String collect = numbers.stream()
+                .map(i -> "(" + Integer.toBinaryString(i) + ")")
+                .map(n -> n.toString())
+                .collect(Collectors.joining("\n"));
+        return collect;
     }
 
     /**
@@ -46,7 +80,12 @@ public class StreamApiMedium {
      * passed with the second input param - 'increment'
      */
     public String charsIncrementation(String string, int increment) {
-        return "";
+        return string.chars()
+                .boxed()
+                .mapToInt(i -> i + increment)
+                .mapToObj(o -> (char) o)
+                .map(c->c.toString()).
+                collect(Collectors.joining());
     }
 
     /**
@@ -54,6 +93,31 @@ public class StreamApiMedium {
      * {"99:Johny", "20:Brad", ...} return the age of the oldest person
      */
     public Long getOldestPersonAge(List<String> people) {
-        return 0L;
+        return people.stream()
+                .map(str -> str.split(":"))
+                .map(f -> Integer.parseInt(f[0]))
+                .max((x, y) -> x - y)
+                .map(f -> (long) f)
+                .get();
+    }
+}
+class Test {
+    public static void main(String[] args) {
+        List<Integer> positiveNumbers = List.of(1, 3, 5, 8);
+        StreamApiMedium sam = new StreamApiMedium();
+        System.out.println(sam.convertAndModifyNumbers(positiveNumbers));
+        List<String> basicInput = List.of("090:John", "-005:Anthony", "000:Nicolas",
+                "037:Brad", "088:Britney", "007:Annie", "091:Jorge", "-120:Bruce",
+                "022:Jasper", "001:Cortney", "050:Emily", "047:Naomi", "090:Leonard");
+        Map<String, Integer> collect = basicInput.stream()
+                .map(str -> str.split(":"))
+                .collect(Collectors.toMap(a -> a[1], a -> Integer.getInteger(a[0]), (a1, a2) -> a1));
+        System.out.println(collect);
+
+//        List<String> basicRecords = List.of("John Stevenson:2020", "Bruce Cage:2015",
+//                "Britney Johns:2005", "Brad Wang:2005", "Lee Stone:2011",
+//                "Rickie Garrison:2015", "Taras Kozak:2017", "John Stevenson:2015",
+//                "Bart Swan:2018", "Brad Wang:2005", "John Stevenson:2015");
+//        System.out.println(sam.getVisitorsPerYear(basicRecords, 2005));
     }
 }
